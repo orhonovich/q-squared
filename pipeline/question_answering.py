8c24924fe527af1438dfe2c9871ca1f06e346e83
orhonovich <77344309+orhonovich@users.noreply.github.com>
2021-01-26 20:19:53 +0200

    update qa pipeline

--- pipeline/question_answering.py
+++ pipeline/question_answering.py
@@ -13,25 +13,40 @@
 # limitations under the License.
 
 import torch
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 
 
-qa_tokenizer = AutoTokenizer.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
-qa_model = AutoModelForQuestionAnswering.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+# qa_tokenizer = AutoTokenizer.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+# qa_model = AutoModelForQuestionAnswering.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+#
+#
+# def get_answer(question, text):  # Code taken from https://huggingface.co/transformers/task_summary.html
+#     inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
+#     input_ids = inputs["input_ids"].tolist()[0]
+#
+#     text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
+#     answer_start_scores, answer_end_scores = qa_model(**inputs)
+#
+#     answer_start = torch.argmax(
+#         answer_start_scores
+#     )  # Get the most likely beginning of answer with the argmax of the score
+#     answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+#
+#     ans = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+#     return ans
+
+model_name = "ktrapeznikov/albert-xlarge-v2-squad-v2"
+
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
 
-def get_answer(question, text):  # Code taken from https://huggingface.co/transformers/task_summary.html
-    inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
-    input_ids = inputs["input_ids"].tolist()[0]
+def get_answer(question, text):
+    QA_input = {
+        'question': question,
+        'context': text
+    }
+    res = nlp(QA_input, handle_impossible_answer=True)
 
-    text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
-    answer_start_scores, answer_end_scores = qa_model(**inputs)
+    return res['answer']
 
-    answer_start = torch.argmax(
-        answer_start_scores
-    )  # Get the most likely beginning of answer with the argmax of the score
-    answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
-
-    ans = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    return ans
 
