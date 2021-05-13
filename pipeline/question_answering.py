8d76a02ca3007cc0b8e9372fa3f79df2e98275e1
Or Honovitch <or.honovitch@cs.huji.ac.il>
2021-05-14 01:11:24 +0300

    paper qa version

--- pipeline/question_answering.py
+++ pipeline/question_answering.py
@@ -16,37 +16,37 @@ import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 
 
-# qa_tokenizer = AutoTokenizer.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
-# qa_model = AutoModelForQuestionAnswering.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
-#
-#
-# def get_answer(question, text):  # Code taken from https://huggingface.co/transformers/task_summary.html
-#     inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
-#     input_ids = inputs["input_ids"].tolist()[0]
-#
-#     text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
-#     answer_start_scores, answer_end_scores = qa_model(**inputs)
-#
-#     answer_start = torch.argmax(
-#         answer_start_scores
-#     )  # Get the most likely beginning of answer with the argmax of the score
-#     answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
-#
-#     ans = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-#     return ans
-
-model_name = "ktrapeznikov/albert-xlarge-v2-squad-v2"
-
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+qa_tokenizer = AutoTokenizer.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+qa_model = AutoModelForQuestionAnswering.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
 
 
-def get_answer(question, text):
-    QA_input = {
-        'question': question,
-        'context': text
-    }
-    res = nlp(QA_input, handle_impossible_answer=True)
+def get_answer(question, text):  # Code taken from https://huggingface.co/transformers/task_summary.html
+    inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
+    input_ids = inputs["input_ids"].tolist()[0]
 
-    return res['answer']
+    text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
+    answer_start_scores, answer_end_scores = qa_model(**inputs)
+
+    answer_start = torch.argmax(
+        answer_start_scores
+    )  # Get the most likely beginning of answer with the argmax of the score
+    answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+
+    ans = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+    return ans
+
+# model_name = "ktrapeznikov/albert-xlarge-v2-squad-v2"
+#
+# nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+#
+#
+# def get_answer(question, text):
+#     QA_input = {
+#         'question': question,
+#         'context': text
+#     }
+#     res = nlp(QA_input, handle_impossible_answer=True)
+#
+#     return res['answer']
 
 
