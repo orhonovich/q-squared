25ab145f0c7211e5b357b8eada55e67278a764a3
Or Honovitch <or.honovitch@cs.huji.ac.il>
2021-05-15 20:09:24 +0300

    update script

--- run_metric.sh
+++ run_metric.sh
@@ -1,11 +1,11 @@
 # Copyright 2020 The Q2 Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,9 @@
 #!/bin/bash
 
 python pipeline/run_pipeline.py \
-      --infile data/dodeca_inconsistent.csv \
+      --infile third_party/data/dodeca_inconsistent.csv \
       --gen_method beam \
       --q_per_cand single \
       --personal remove \
-      --outfile dodeca_dev_no_hallu_no_coref_beam_single_remove.csv
+      --outfile dodeca_inconsistent_out \
+      --save_steps
